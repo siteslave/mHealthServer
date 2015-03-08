d9d3c79c8deaffb9bf394dc3e959b900c9479c80
Satit Rianpit <rianpit@gmail.com>
2015-03-08 18:09:58 +0700

    confirm typearea

--- models/Typearea.js
+++ models/Typearea.js
@@ -5,7 +5,7 @@ exports.list = function (db, hospcode) {
 
     var q = Q.defer();
 
-    db.raw('SELECT a.*,b.confirm_hospcode FROM (SELECT a.HOSPCODE,a.`NAME`,a.SEX,a.BIRTH,a.CID, a.TYPEAREA,count(*)AS total FROM person a WHERE a.TYPEAREA IN("1", "3") GROUP BY a.CID HAVING total > 1 AND a.HOSPCODE = ? ORDER BY total DESC) a LEFT JOIN confirm_typearea b ON a.CID = b.cid', [hospcode])
+    db.raw('SELECT a.*,b.confirm_hospcode FROM (SELECT a.HOSPCODE,a.NAME, a.LNAME,a.SEX,a.BIRTH,a.CID, a.TYPEAREA,count(*)AS total FROM person a WHERE a.TYPEAREA IN("1", "3") GROUP BY a.CID HAVING total > 1 AND a.HOSPCODE = ? ORDER BY total DESC) a LEFT JOIN confirm_typearea b ON a.CID = b.cid', [hospcode])
         .then(function (rows){
             q.resolve(rows[0]);
         });
@@ -14,16 +14,10 @@ exports.list = function (db, hospcode) {
 
 exports.detail = function (db, cid) {
     var q = Q.defer();
-
-    /*
-     SELECT HOSPCODE,chospital.hosname,CID,`NAME`,LNAME,SEX,BIRTH,TYPEAREA,D_UPDATE FROM person
-     LEFT JOIN chospital ON person.HOSPCODE=chospital.hoscode
-     WHERE CID='1449900384355'
-     AND TYPEAREA in('1','3')
-     */
     db('person as p')
-        .select('p.HOSPCODE', 'c.hosname' ,'p.CID', 'p.NAME', 'p.LNAME', 'p.SEX', 'p.BIRTH' ,'p.TYPEAREA' ,'p.D_UPDATE')
+        .select('p.HOSPCODE', 'c.hosname' ,'p.CID', 'p.NAME', 'p.LNAME', 'p.SEX', 'p.BIRTH' ,'p.TYPEAREA' ,'p.D_UPDATE', 'c.confirm_hospcode')
         .leftJoin('chospital as c', 'p.HOSPCODE', 'c.hoscode')
+        .leftJoin('confirm_typearea as t', 't.cid', 'p.CID')
         .where('p.CID', cid)
         .whereIn('p.TYPEAREA', ['1', '3'])
         .exec(function (err, rows) {
